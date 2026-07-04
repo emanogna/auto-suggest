697a90b5894e4518f9ca5ebba1d877cb9ccd3eac
Manogna <24wh1a05r1@bvrithyderabad.edu.in>
2026-07-04 09:57:48 +0530

    added random user

--- frontend/index.js
+++ frontend/index.js
@@ -13,6 +13,9 @@ const users = [
 
 let curUserId = 0;
 function toggle(){
+
+    //console.log("frontend");
+
     if(curUserId == 0)
         curUserId = 1;
     else
@@ -23,6 +26,36 @@ function toggle(){
 
     //name
     document.getElementById("card-name").innerText=users[curUserId].name;
+
+    //gender
+    document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
+
+function getRandomUser(){
+    fetch("https://randomuser.me/api")
+        .then(function (data){
+            return data.json()
+        })
+        .then(function (parsedData){
+            //console.log(parsedData)
+            let gender = parsedData.results[0].gender;
+
+            let first = parsedData.results[0].name.first;
+            let last = parsedData.results[0].name.last;
+
+            let name = first + " " + last;
+            //console.log("name");
+
+            let imageUrl = parsedData.results[0].picture.large;
+
+            //gender
+            document.getElementById("card-gender").innerText=gender;
+            //name
+            document.getElementById("card-name").innerText=name
+            //image
+            document.getElementById("img").src=imageUrl
+
+        })
 }
 
 console.log("Hello from JS");
